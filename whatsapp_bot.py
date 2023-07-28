17818600c8a0b75156ee44db5aecc9b81e8f0a71
ajexjoshy <ajexjoshyclass8@gmail.com>
2023-07-28 12:31:16 +0530

    whatsapp bot

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -1,1 +1,36 @@
-print('hello')
+from tkinter import *
+root = Tk()
+root.geometry('400x500')
+root.title('Whatsapp Bot')
+import pywhatkit
+
+e1=Entry(root)
+e2=Entry(root)
+e1.insert(0, "+91")
+e3=Entry(root)
+e4=Entry(root)
+
+
+l1=Label(root,text='Enter phone Number')
+l2=Label(root,text='Enter your message')
+l3=Label(root,text='Enter hour in which message has to be sent (24 hour)')
+l4=Label(root,text='Enter minute in which message has to be sent')
+
+
+l1.pack()
+e1.pack()
+l2.pack()
+e2.pack()
+l3.pack()
+e3.pack()
+l4.pack()
+e4.pack()
+
+def prgrm():
+    pywhatkit.sendwhatmsg(e1.get(),e2.get(),int(e3.get()),int(e4.get()))
+
+
+Button(root,text='Submit',command=prgrm).pack()
+
+
+root.mainloop()
